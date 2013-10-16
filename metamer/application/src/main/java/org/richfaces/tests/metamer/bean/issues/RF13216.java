5c0d8208ca24184d03633f637b1393db6c773a7a
Juraj Huska <jhuska@redhat.com>
2013-10-16 12:26:38 +0200

    RF-13216: added another pages for reproduction

--- metamer/application/src/main/java/org/richfaces/tests/metamer/bean/issues/RF13216.java
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/issues/RF13216.java
@@ -1,14 +1,35 @@
 package org.richfaces.tests.metamer.bean.issues;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "rf13216")
-@RequestScoped
+@SessionScoped
 public class RF13216 {
+
+    private boolean showPopup = false;
+
+    public RF13216() {
+        System.out.println("New bean created!");
+    }
 
     public String action() {
         System.out.println("action called!");
         return "";
+    }
+
+    public String switchPopup() {
+        showPopup = !showPopup;
+        System.out.println("Switched popup called! Showpopup property set to " + showPopup);
+        return "";
+    }
+
+    public boolean isShowPopup() {
+        System.out.println("isShowPopup called!");
+        return showPopup;
+    }
+
+    public void setShowPopup(boolean showPopup) {
+        this.showPopup = showPopup;
     }
 }
