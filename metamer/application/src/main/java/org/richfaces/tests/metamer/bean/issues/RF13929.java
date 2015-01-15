2013f957f1c2ebec063ccd86004b93aa81b55514
Jiri Stefek <jstefek@redhat.com>
2015-01-15 11:58:51 +0100

    metamer: push: issue reproducer with TopicsContext

--- metamer/application/src/main/java/org/richfaces/tests/metamer/bean/issues/RF13929.java
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/issues/RF13929.java
@@ -34,6 +34,8 @@ import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
 import javax.jms.Topic;
 
+import org.richfaces.application.push.MessageException;
+import org.richfaces.application.push.TopicKey;
 import org.richfaces.application.push.TopicsContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,53 +50,100 @@ public class RF13929 implements Serializable {
     private static final String JMS_ADDRESS = "jmsSampleAddress1";
     private static final String JMS_ADDRESS_JNDI = "topic/jmsSampleAddress1";
     private static final Logger LOGGER = LoggerFactory.getLogger(RF13929.class);
+    private static final String PUSH_TOPICS_CONTEXT_ADDRESS_1 = "topic1@tcSampleAddress1";
+    private static final String PUSH_TOPICS_CONTEXT_ADDRESS_2 = "topic2@tcSampleAddress1";
+    private static final String PUSH_TOPICS_CONTEXT_ADDRESS_3 = "tcSampleAddress1";
     private static final String SUBTOPIC1_ADDRESS = "subtopic1";
     private static final String SUBTOPIC2_ADDRESS = "subtopic2";
     private static final long serialVersionUID = 1L;
-
     @Inject
     private transient JMSContext jmsContext;
     @Resource(mappedName = JMS_ADDRESS_JNDI)
     private Topic jmsTopic;
 
-    private int update1 = 0;
-    private int update2 = 0;
-    private int update3 = 0;
+    private transient TopicsContext topicsContext;
 
-    public String getAddress1() {
+    private int update1;
+    private int update2;
+    private int update3;
+
+    public String getJMSAddress1() {
         return MessageFormat.format("{0}@{1}", SUBTOPIC1_ADDRESS, JMS_ADDRESS);
     }
 
-    public String getAddress2() {
+    public String getJMSAddress2() {
         return MessageFormat.format("{0}@{1}", SUBTOPIC2_ADDRESS, JMS_ADDRESS);
     }
 
-    public String getAddress3() {
+    public String getJMSAddress3() {
         return JMS_ADDRESS;
+    }
+
+    private TopicsContext getTopicsContext() {
+        if (topicsContext == null) {
+            topicsContext = TopicsContext.lookup();
+        }
+        return topicsContext;
+    }
+
+    public String getTopicsContextAddress1() {
+        return PUSH_TOPICS_CONTEXT_ADDRESS_1;
+
+    }
+
+    public String getTopicsContextAddress2() {
+        return PUSH_TOPICS_CONTEXT_ADDRESS_2;
+    }
+
+    public String getTopicsContextAddress3() {
+        return PUSH_TOPICS_CONTEXT_ADDRESS_3;
     }
 
     @PostConstruct
     public void init() {
-        TopicsContext.lookup();
+        update1 = 0;
+        update2 = 0;
+        update3 = 0;
     }
 
-    public void pushJMS1() throws JMSException {
+    public void pushWithJMS1() throws JMSException {
         ObjectMessage message = jmsContext.createObjectMessage(MessageFormat.format("data #{0}", ++update1));
+        // subtopic workaround https://developer.jboss.org/wiki/CreatingSubtopicWithJMSPush
         message.setStringProperty("rf_push_subtopic", SUBTOPIC1_ADDRESS);
         jmsContext.createProducer().send(jmsTopic, message);
-        LOGGER.error("jms push event 1");
+        LOGGER.debug("jms push event 1");
     }
 
-    public void pushJMS2() throws JMSException {
+    public void pushWithJMS2() throws JMSException {
         ObjectMessage message = jmsContext.createObjectMessage(MessageFormat.format("data #{0}", ++update2));
+        // subtopic workaround https://developer.jboss.org/wiki/CreatingSubtopicWithJMSPush
         message.setStringProperty("rf_push_subtopic", SUBTOPIC2_ADDRESS);
         jmsContext.createProducer().send(jmsTopic, message);
-        LOGGER.error("jms push event 2");
+        LOGGER.debug("jms push event 2");
     }
 
-    public void pushJMS3() throws JMSException {
+    public void pushWithJMS3() throws JMSException {
         ObjectMessage message = jmsContext.createObjectMessage(MessageFormat.format("data #{0}", ++update3));
+        // no subtopic
         jmsContext.createProducer().send(jmsTopic, message);
-        LOGGER.error("jms push event 3");
+        LOGGER.debug("jms push event 3");
+    }
+
+    public void pushWithTopicsContext1() throws MessageException {
+        TopicKey topicKey = new TopicKey(getTopicsContextAddress1());
+        getTopicsContext().publish(topicKey, MessageFormat.format("data #{0}", ++update1));
+        LOGGER.debug("push event 1");
+    }
+
+    public void pushWithTopicsContext2() throws MessageException {
+        TopicKey topicKey = new TopicKey(getTopicsContextAddress2());
+        getTopicsContext().publish(topicKey, MessageFormat.format("data #{0}", ++update2));
+        LOGGER.debug("push event 2");
+    }
+
+    public void pushWithTopicsContext3() throws MessageException {
+        TopicKey topicKey = new TopicKey(getTopicsContextAddress3());
+        getTopicsContext().publish(topicKey, MessageFormat.format("data #{0}", ++update3));
+        LOGGER.debug("push event 3");
     }
 }
